3c38a0f0ed410f36d4a800e0d8202a83685e6c6a
Arsalan Inam <m.arsalaninam@gmail.com>
2023-03-27 03:51:32 +0200

    Add constants

---
+++ src/main/java/org/coop/constant/ScenarioNameConstant.java
@@ -0,0 +1,11 @@
+package org.coop.constant;
+
+public interface ScenarioNameConstant {
+
+    String VALIDATE_STATUS_CODE_200 =
+            "Validate Response Status Code is 200 : ";
+
+    String VALIDATE_SUCCESS_RESPONSE_BODY = "Validate Success Response Body of API : ";
+
+    String VALIDATE_ERROR_RESPONSE_BODY = "Validate Error Response Body of API : ";
+}
---
+++ src/main/java/org/coop/constant/ServiceConstant.java
@@ -0,0 +1,26 @@
+package org.coop.constant;
+
+public interface ServiceConstant {
+
+    String BASE_URL = "baseURL";
+
+    String TOKEN_ENDPOINT = "tokenEndpoint";
+
+    String CLIENT_ID = "clientId";
+
+    String CLIENT_SECRET = "clientSecret";
+
+    String VALID_USER_ID = "validUserId";
+    String INVALID_USER_ID = "invalidUserId";
+
+    String GRANT_TYPE = "grantType";
+    String BARN_UNLOCK_ENDPOINT = "barnEndpoint";
+
+    String TOILET_SEAT_DOWN_ENDPOINT = "toiletSeatDownEndpoint";
+
+    String FEED_CHICKEN_ENDPOINT = "feedYourChickenEndpoint";
+
+    String COLLECT_EGGS_ENDPOINT = "collectEggsEndpoint";
+
+    String COUNT_EGGS_ENDPOINT = "countEggsEndpoint";
+}
